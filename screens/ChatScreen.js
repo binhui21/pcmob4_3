3d040aba58a760ed8877b9bad2f1a9af17ff7e87
binhui <teopinghwee@gmail.com>
2021-05-30 21:03:06 +0800

    final commit

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -5,24 +5,41 @@ import firebase from "../database/firebaseDB";
 import {MaterialCommunityIcons } from "@expo/vector-icons";
 
 const db = firebase.firestore().collection("messages");
+const auth=firebase.auth();
+const anonymousUser = { name: "Anonymous", id: "1A" };
 
 export default function ChatScreen({navigation}) {
 
     const [messages, setMessages] = useState([]);
+    const [currentUser, setCurrentUser] = useState(anonymousUser);
 
     useEffect(() => {
         const unsubscribe = db
             .orderBy("createAt", "desc")
             .onSnapshot((collectionSnapshot) => {
-                const serverMessages = collectionSnapshot.docs.map((doc) => doc.data());
+                const serverMessages = collectionSnapshot.docs.map((doc) => {
+                    const data = doc.data();
+                    console.log(data);
+                    
+                    const jsDate = new Date(data.createAt.seconds = 1000);
+                    
+                    const newDoc = {
+                        ...data,
+                        createAt: jsDate,
+                    };
+                    return newDoc;
+                });
                 setMessages(serverMessages);
             });
-        firebase.auth().onAuthStateChanged((user) => {
-            if (user) {
-                navigation.navigate("Chat", { id: user.id, email: user.email });
-            } else {
-                navigation.navigate("Login");
-            }
+
+            firebase.auth().onAuthStateChanged((user) => {
+                if (user) {
+                  navigation.navigate("Chat");
+                  setCurrentUser({ id: user.uid, name: user.email });
+                } else {
+                  navigation.navigate("Login");
+                  setCurrentUser(anonymousUser);
+                }
         });
 
         navigation.setOptions({
@@ -57,9 +74,13 @@ export default function ChatScreen({navigation}) {
         <GiftedChat
           messages={messages}
           onSend={messages => onSend(messages)}
-          user={{
-            _id: 1,
-          }}
+          //renderUsernameOnMessage={true}
+         // listViewProps={{ style: { backgroundColor: "#777" }}}
+          user={currentUser}
+          //{{
+            //_id: firebase.auth().currentUser.uid,
+            //name: firebase.auth().currentUser.email,
+          //}}
         />
       );
 }
